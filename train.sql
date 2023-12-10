ff4ba6511e4b2c00375470d7628556fe0cd38a06
Codgm <151416805+Codgm@users.noreply.github.com>
2023-12-10 17:34:46 +0900

    Update train.sql

--- train.sql
+++ train.sql
@@ -420,3 +420,141 @@ CREATE TABLE IF NOT EXISTS `train`.`maintenance_record` (
   );
 
 #drop table employees;
+
+-- Station 테이블에 데이터 삽입
+DELIMITER //
+CREATE PROCEDURE InsertStation(IN StationName VARCHAR(255), IN Location VARCHAR(255), IN PlatformCount INT)
+BEGIN
+    INSERT INTO Station(StationName, Location, PlatformCount) VALUES (StationName, Location, PlatformCount);
+END //
+DELIMITER ;
+
+-- DetailedRoute 테이블에 새로운 레코드가 삽입될 때마다 Connected_Route 테이블에 대응하는 레코드가 자동으로 삽입
+DELIMITER //
+CREATE TRIGGER AfterInsertDetailedRoute
+AFTER INSERT ON DetailedRoute
+FOR EACH ROW
+BEGIN
+    DECLARE StartStation INT;
+    DECLARE EndStation INT;
+    
+    SELECT StartID, EndID INTO StartStation, EndStation FROM DetailedRoute WHERE DetaliedRouteID = NEW.DetaliedRouteID;
+    
+    INSERT INTO Connected_Route(RouteID, num, DetaliedRouteID)
+    VALUES (NEW.DetaliedRouteID, 1, NEW.DetaliedRouteID),
+           (NEW.DetaliedRouteID, 2, StartStation),
+           (NEW.DetaliedRouteID, 3, EndStation);
+END //
+DELIMITER ;
+
+-- Promotion 및 Fare에 데이터를 삽입할 때 자동으로 할인 및 요금 정보를 적용
+DELIMITER //
+CREATE PROCEDURE ApplyPromotionAndFare(IN promoID INT, IN fareID INT)
+BEGIN
+    DECLARE discountValue INT;
+    DECLARE fareValue INT;
+    
+    -- Promotion 테이블에서 할인율 조회
+    SELECT discount INTO discountValue FROM Promotion WHERE PromotionID = promoID;
+    
+    -- Fare 테이블에서 기본 요금 조회
+    SELECT Fare INTO fareValue FROM Fare WHERE FareID = fareID;
+    
+    -- 할인된 요금을 Fare 테이블에 업데이트
+    UPDATE Fare SET Fare = fareValue - (fareValue * discountValue / 100) WHERE FareID = fareID;
+END //
+DELIMITER ;
+
+-- 트리거 생성
+DELIMITER //
+CREATE TRIGGER AfterInsertPromotionAndFare
+AFTER INSERT ON Promotion
+FOR EACH ROW
+BEGIN
+    DECLARE fareID INT;
+    
+    -- Fare 테이블에 새로운 일반 요금 추가
+    INSERT INTO Fare (Fare) VALUES (10000);
+    
+    -- 방금 추가된 Fare의 FareID 가져오기
+    SELECT MAX(FareID) INTO fareID FROM Fare;
+    
+    -- 새로 추가된 Promotion과 Fare 정보를 프로시저를 통해 업데이트
+    CALL ApplyPromotionAndFare(NEW.PromotionID, fareID);
+END //
+DELIMITER ;
+
+-- 새로운 화물 데이터가 삽입될 때 자동으로 프로시저와 트리거를 사용하여 결제 정보를 추가
+DELIMITER //
+CREATE PROCEDURE CreateCargoPayment(IN cargoID INT, IN routeID INT, IN fareID INT, IN paymentAmount DECIMAL(10, 2), IN paymentDate DATE)
+BEGIN
+    -- 화물 결제 정보를 CargoPayment 테이블에 추가
+    INSERT INTO CargoPayment (CargoID, RouteID, FareID, PaymentAmount, PaymentDate)
+    VALUES (cargoID, routeID, fareID, paymentAmount, paymentDate);
+    
+    -- 화물 결제 정보를 Payment 테이블에 추가
+    INSERT INTO Payment (CargoID, RouteID, FareID, PaymentAmount, PaymentDate)
+    VALUES (cargoID, routeID, fareID, paymentAmount, paymentDate);
+END //
+DELIMITER ;
+
+-- 트리거 생성
+DELIMITER //
+CREATE TRIGGER AfterInsertCargo
+AFTER INSERT ON Cargo
+FOR EACH ROW
+BEGIN
+    DECLARE routeID INT;
+    DECLARE fareID INT;
+    
+    -- Cargo, Route 및 Fare 테이블에 새로운 데이터 추가
+    SET routeID = 1; -- 적절한 노선 ID로 설정
+    SET fareID = 1;  -- 적절한 요금 ID로 설정
+    
+    -- 새로 추가된 Cargo 및 관련 정보를 프로시저를 통해 결제 정보로 추가
+    CALL CreateCargoPayment(NEW.CargoID, routeID, fareID, NEW.CargoWeight * NEW.CargoType, CURRENT_DATE);
+END //
+DELIMITER ;
+
+-- 새로운 물품 분실 정보 삽입 시 처리
+DELIMITER //
+CREATE PROCEDURE HandleLostItem(IN ownerName VARCHAR(255), IN itemInfo TEXT, IN dateInfo DATE, IN foundLocation INT, IN processingStatus VARCHAR(50))
+BEGIN
+    -- 물품 분실 정보 삽입
+    INSERT INTO LostItems (OwnerName, ItemInfo, DateInfo, FoundLocation, ProcessingStatus)
+    VALUES (ownerName, itemInfo, dateInfo, foundLocation, processingStatus);
+END //
+DELIMITER ;
+
+--  새로운 사고 정보 삽입 시 처리
+CREATE PROCEDURE HandleAccident(IN stationID INT, IN accidentTime DATETIME, IN weather VARCHAR(255), 
+    IN accidentType VARCHAR(255), IN injuredCount INT, IN actionTaken TEXT)
+BEGIN
+    -- 사고 정보 삽입
+    INSERT INTO AccidentInfo (StationID, AccidentTime, Weather, AccidentType, InjuredCount, ActionTaken)
+    VALUES (stationID, accidentTime, weather, accidentType, injuredCount, actionTaken);
+END //
+DELIMITER ;
+
+-- LostItems 테이블에 새로운 데이터가 삽입될 때 처리
+DELIMITER //`
+CREATE TRIGGER AfterInsertLostItem
+AFTER INSERT ON LostItems
+FOR EACH ROW
+BEGIN
+    -- 새로 추가된 LostItems 데이터에 대한 처리 프로시저 호출
+    CALL HandleLostItem(NEW.OwnerName, NEW.ItemInfo, NEW.DateInfo, NEW.FoundLocation, NEW.ProcessingStatus);
+END //
+DELIMITER ;    
+
+-- AccidentInfo 테이블에 새로운 데이터가 삽입될 때 처리
+DELIMITER //
+CREATE TRIGGER AfterInsertAccident
+AFTER INSERT ON AccidentInfo
+FOR EACH ROW
+BEGIN
+    -- 새로 추가된 AccidentInfo 데이터에 대한 처리 프로시저 호출
+    CALL HandleAccident(NEW.StationID, NEW.AccidentTime, NEW.Weather, NEW.AccidentType, NEW.InjuredCount, NEW.ActionTaken);
+END //
+DELIMITER ;
+
